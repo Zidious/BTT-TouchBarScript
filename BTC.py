d3064fdbec7a1bd4dba39acdc205b75bdba10c7f
Gabe <41127686+Zidious@users.noreply.github.com>
2019-04-14 19:02:50 +0100

    Add Moonlight (LX) Script

--- BTC.py
+++ BTC.py
@@ -6,10 +6,10 @@ coinGecko = CoinGeckoAPI()
 
 # AVA FUNCTION - PRICE, 24HVOL
 def getBTCPrice():
-    getAVA = coinGecko.get_coins_markets(ids='bitcoin',
+    getBTC = coinGecko.get_coins_markets(ids='bitcoin',
                                          vs_currency='usd')
-    currentPrice = "{:,}".format(round(getAVA[0]['current_price'], 2))
-    totalVolume = "{:,}".format(round(getAVA[0]['total_volume'], 2))
+    currentPrice = "{:,}".format(round(getBTC[0]['current_price'], 2))
+    totalVolume = "{:,}".format(round(getBTC[0]['total_volume'], 2))
     return f"BTC: ${currentPrice} - " \
         f"24H VOL: ${totalVolume}"
 
